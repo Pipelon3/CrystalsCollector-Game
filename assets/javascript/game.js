24cda91731fdbfe0a1918cc7c1ef2fd22a8e0d90
Pipelon3 <Pipelon999@gmail.com>
2019-06-06 17:11:01 -0600

    semi-working build

---
+++ assets/javascript/game.js
@@ -0,0 +1,95 @@
+var random_result;
+var lost = 0;
+var win = 0;
+var previous = 0;
+
+var resetAndStart = function () {
+
+	$(".crystals").empty();
+
+	var images = [
+			'./images/blue_crystal.jpg', 
+			'', 
+			'', 
+			''];
+		
+	random_result = Math.floor(Math.random() * 101 ) + 19; 
+
+
+	$("#result").html('Goal: ' + random_result);
+
+	for(var i = 0; i < 4; i++){
+
+		var random = Math.floor(Math.random() * 11) + 1;
+
+		var crystal = $("<div>");
+			crystal.attr({
+				"class": 'crystal',
+				"data-random": random
+			});
+			crystal.css({
+				"background-image":"url('" + images[i] + "')",
+				"background-size":"cover"
+
+			});
+
+
+		$(".crystals").append(crystal);
+
+	}
+
+	$("#previous").html("Total Score: " + previous);
+
+}
+
+
+resetAndStart();
+
+
+// On click events
+
+$(document).on('click', ".crystal", function () {
+	var num = parseInt($(this).attr('data-random'));
+
+	previous += num;
+
+
+	$("#previous").html("Total score: " + previous);
+
+	console.log(previous);
+
+	if(previous > random_result){
+
+		lost++;
+
+		$("#lost").html("Losses: " + lost);
+
+		previous = 0;
+
+		resetAndStart();
+
+	} 
+	else if(previous === random_result){
+
+		win++;
+
+		$("#win").html("Wins: " + win);
+
+		previous = 0;
+
+		resetAndStart();
+
+	}
+
+});
+
+// Sound on-clicks
+
+
+
+
+
+
+
+
+
